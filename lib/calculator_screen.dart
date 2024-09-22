97fd1b515c931755f4c0dbc801d8892b1b57d22f
Nipuna Madula <nipunamadula2001@gmail.com>
2024-09-23 00:15:40 +0530

    update file

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -10,6 +10,14 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:Column(
+        children: [
+          //output
+          Text("0"),
+
+        ],
+        ),
+    );
   }
 }
