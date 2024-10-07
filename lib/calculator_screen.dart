59eeb0458b96c012d4f8f9e6295209e09fa351b7
Nipuna Madula <nipunamadula2001@gmail.com>
2024-10-07 21:54:25 +0530

    Adjust layout for screen width responsiveness

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -38,7 +38,9 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             children: Btn.buttonValues
             .map(
               (value) => SizedBox(
-                width: screenSize.width/4,
+                width: value==Btn.n0
+                ?screenSize.width/2
+                : (screenSize.width/4),
                 height: screenSize.width/5 ,
                 child: buildbutton(value),
               ),
@@ -56,18 +58,7 @@ Widget buildbutton(value) {
   return Padding(
     padding: const EdgeInsets.all(4.0),
     child: Material(
-      color: [Btn.del,Btn.clr].contains(value)
-      ?Colors.blueGrey
-      :[
-        Btn.per,
-        Btn.multiply,
-        Btn.add,
-        Btn.subtract,
-        Btn.divide,
-        Btn.calculate
-        ].contains(value)
-        ?Colors.orange
-        :Colors.black87,
+      color: getBtnColor(value),
       clipBehavior: Clip.hardEdge,
       shape: OutlineInputBorder(
         borderSide: BorderSide(color: Colors.white24),
@@ -83,6 +74,21 @@ Widget buildbutton(value) {
         )),
     ),
   );
+}
+
+Color getBtnColor(value){
+  return [Btn.del,Btn.clr].contains(value)
+      ?Colors.blueGrey
+      :[
+        Btn.per,
+        Btn.multiply,
+        Btn.add,
+        Btn.subtract,
+        Btn.divide,
+        Btn.calculate
+        ].contains(value)
+        ?Colors.orange
+        :Colors.black87;
 }
 
 }
